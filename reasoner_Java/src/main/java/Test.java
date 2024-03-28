2b40148ea0080a2cb71bda89c6dfab3b1b2f35ec
maximemaire <maxime.maire104@orange.fr>
2024-03-28 15:21:40 +0100

    Avancement: affichage des axiomes de l'ontologie ave JAVA

--- reasoner_Java/src/main/java/Test.java
+++ reasoner_Java/src/main/java/Test.java
@@ -1,57 +1,67 @@
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
+import org.semanticweb.owlapi.reasoner.OWLReasonerRuntimeException;
 import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.reasoner.NodeSet;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.semanticweb.HermiT.Reasoner.ReasonerFactory;
 
 import java.io.InputStream;
+import java.util.Set;
 
 public class Test {
     public void loadOntology() throws OWLOntologyCreationException {
         OWLOntologyManager om = OWLManager.createOWLOntologyManager();
         InputStream inputStream = getClass().getResourceAsStream("/onto_herelles.owl");
-        OWLOntology pressInnovOntology = om.loadOntologyFromOntologyDocument(inputStream);
-        checkConsistencyAndInfer(pressInnovOntology);
+        OWLOntology onto = om.loadOntologyFromOntologyDocument(inputStream);
+        checkConsistencyAndInfer(onto);
     }
 
     public void checkConsistencyAndInfer(OWLOntology ontology) {
         OWLReasonerFactory reasonerFactory = new ReasonerFactory();
-        OWLReasonerConfiguration config = new SimpleConfiguration();
-        OWLReasoner reasoner = reasonerFactory.createReasoner(ontology, config);
+        OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
 
-        try {
-            reasoner.precomputeInferences();
+
+        if (reasoner.isConsistent()) {
             System.out.println("L'ontologie est cohérente.");
 
-            for (OWLClass cls : ontology.getClassesInSignature()) {
-                NodeSet<OWLClass> subClasses = reasoner.getSubClasses(cls, true);
-                for (Node<OWLClass> subClassNode : subClasses) {
-                    for (OWLClass subClass : subClassNode.getEntities()) {
-                        System.out.println("Classe " + cls.getIRI().getShortForm() + " a pour sous-classe : " + subClass.getIRI().getShortForm());
-                    }
-                }
+            /*
+            Ca ne donne que les axiomes qui existent deja dans l'ontologie, je souhaite récuperer les axiomes inferrer
+            sans avoir les aximes de base.
+            */
+
+            //Pas de doublons dans les Set :
+            //Set<OWLClassAssertionAxiom> inferredAxioms = ontology.getAxioms(AxiomType.CLASS_ASSERTION);
+            //peut avor des doublons dans les listes :
+            List<OWLClassAssertionAxiom> inferredAxioms = new ArrayList<>(ontology.getAxioms(AxiomType.CLASS_ASSERTION));
+
+            for (OWLClassAssertionAxiom axiom : inferredAxioms) {
+                System.out.println("Inferred axiom: " + axiom);
             }
-        } catch (OWLRuntimeException e) {
-            System.err.println(e.getMessage());
+            printSubclassesAndInstance(reasoner);
+        } else {
+            System.out.println("L'ontologie est inconsistante. Voici les instances à l'origine de cette inconsistence :");
+            printInconsistentInstances(reasoner);
         }
 
-        
-        // for (OWLClass cls : ontology.getClassesInSignature()) {
-        //     if (!reasoner.isSatisfiable(cls)) {
-        //         System.out.println("Classe incohérente : " + cls.getIRI().getShortForm());
-        //     }
-        // }
-        // System.out.println("Inférences :");
-
-
+        reasoner.dispose();
     }
 
+    private void printSubclassesAndInstance(OWLReasoner reasoner) {
+        // A voir plus tard
+    }
+
+    private void printInconsistentInstances(OWLReasoner reasoner) {
+        // A voir plus tard
+    }
+    
 
     public static void main(String[] args) {
         Test test = new Test();
